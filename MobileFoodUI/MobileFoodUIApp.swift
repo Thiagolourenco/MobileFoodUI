a096bd2bf56f4c0fbe654f9065b43e8d71bc6441
Thiago Lourenço <thiago.lourenco@gok.digital>
2023-10-05 13:39:51 -0300

    Initial Commit

---
+++ MobileFoodUI/MobileFoodUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileFoodUIApp.swift
+//  MobileFoodUI
+//
+//  Created by Thiago Lourenço on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MobileFoodUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
